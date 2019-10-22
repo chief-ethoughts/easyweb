1662e89cb2b15935d240a9e7c04df4fd62b9cd05
Harikumar Gokulam <Harikumar.Gokulam@marlabs.com>
2019-10-22 19:25:40 +0530

    Creating base files and directories

---
+++ src/test/java/com/girah/sweb/WebCore/WebCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.girah.sweb.WebCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
